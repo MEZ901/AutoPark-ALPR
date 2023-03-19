3e803de7f3f91a0a7d43647917c733cad8c515ce
issam <issammez44@gmail.com>
2023-03-19 17:28:58 +0100

    config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmNax93m42yeGy702wmK4mZI4MH7Z-_44",
+  authDomain: "autopark-9b3a5.firebaseapp.com",
+  projectId: "autopark-9b3a5",
+  storageBucket: "autopark-9b3a5.appspot.com",
+  messagingSenderId: "1094860702797",
+  appId: "1:1094860702797:web:fdda79727a7a700c438804",
+  measurementId: "G-SJ2ZFMVRR4"
+};
+
+const app = initializeApp(firebaseConfig);
