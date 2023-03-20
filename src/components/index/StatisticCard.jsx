7375265cd6bca8b5322534f01d7e7e1b171829ae
issam <issammez44@gmail.com>
2023-03-20 13:54:59 +0100

    update the icons and links in footer section

--- src/components/index/StatisticCard.jsx
+++ src/components/index/StatisticCard.jsx
@@ -1,8 +1,8 @@
 const StatisticCard = ({stat}) => {
   return (
     <>
-        <dt class="mb-2 text-3xl md:text-4xl font-extrabold">{stat.num}</dt>
-        <dd class="font-light text-gray-500 dark:text-gray-400">{stat.title}</dd>   
+        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">{stat.num}</dt>
+        <dd className="font-light text-gray-500 dark:text-gray-400">{stat.title}</dd>   
     </>
   )
 }
--- src/components/index/Statistics.jsx
+++ src/components/index/Statistics.jsx
@@ -16,9 +16,9 @@ const Statistics = () => {
     }
   ];
   return (
-    <section class="bg-white dark:bg-gray-900">
-      <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
-          <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
+    <section className="bg-white dark:bg-gray-900">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+          <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
             {stats.map((stat, index) => (
               <div key={index}>
                 <StatisticCard stat={stat} />
