b98d8ce71d841be7e450d3df363e1d90bc3edbf8
issam <issammez44@gmail.com>
2023-03-21 15:04:21 +0100

    add register page

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -6,31 +6,31 @@ import TextField from '@mui/material/TextField';
 
 const Login = () => {
   return (
-    <div class="min-h-screen py-6 flex flex-col justify-center sm:py-12">
-        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
             <div
-                class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
             </div>
-            <div class="relative px-4 py-10 bg-gray-100 shadow-lg sm:rounded-3xl sm:p-20">
-                <div class="w-96 mx-auto">
+            <div className="relative px-4 py-10 bg-gray-100 shadow-lg sm:rounded-3xl sm:p-20">
+                <div className="mx-auto">
                     <div>
-                        <h1 class="text-2xl font-semibold mb-5">Login</h1>
+                        <h1 className="text-2xl font-semibold mb-5">Login</h1>
                     </div>
                     <Button variant="outlined" className="w-full" startIcon={<GoogleIcon />}>Login with Google</Button>
                     <Divider className="pt-5"> OR </Divider>
-                    <div class="divide-y divide-gray-200">
-                        <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                            <div class="relative">
-                                <TextField id="filled-basic" label="Email" variant="filled" fullWidth />
+                    <div className="divide-y divide-gray-200">
+                        <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                            <div>
+                                <TextField label="Email" variant="filled" fullWidth />
                             </div>
-                            <div class="relative">
-                                <TextField id="filled-basic" label="Password" variant="filled" fullWidth />
+                            <div>
+                                <TextField label="Password" variant="filled" fullWidth />
                             </div>
-                            <div class="relative">
-                                <Button variant="contained" color="primary" fullWidth>Submit</Button>
+                            <div>
+                                <Button variant="contained" color="primary" fullWidth>Login</Button>
                             </div>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don't have an account? <Link to="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Register here</Link>
+                            <p className="text-sm font-light text-gray-500">
+                                Don't have an account? <Link to="/register" className="font-medium text-primary-600 hover:underline">Register here</Link>
                             </p>
                         </div>
                     </div>
