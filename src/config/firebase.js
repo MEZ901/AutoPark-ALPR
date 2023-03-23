a48f8b670dcaf9b3491014daba80521e7a9e5c2d
issam <issammez44@gmail.com>
2023-03-23 06:18:02 +0100

    add .env.example so it's will be easy to people to set up thier variables environment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmNax93m42yeGy702wmK4mZI4MH7Z-_44",
-  authDomain: "autopark-9b3a5.firebaseapp.com",
-  projectId: "autopark-9b3a5",
-  storageBucket: "autopark-9b3a5.appspot.com",
-  messagingSenderId: "1094860702797",
-  appId: "1:1094860702797:web:fdda79727a7a700c438804",
-  measurementId: "G-SJ2ZFMVRR4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
